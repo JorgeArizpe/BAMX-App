ee02bfb08d6503aed54f0dc29932341aad54e228
Jorge Arizpe <jorgeantonio.arizpe@gmail.com>
2024-09-24 07:42:52 -0600

    fecthing categorias from db

--- screens/Inventario.tsx
+++ screens/Inventario.tsx
@@ -1,12 +1,37 @@
 import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
 import CategoryItem from '../components/CategoryItem';
+import { useFirebase } from '../db/FirebaseContext';
+import { doc, getDoc } from 'firebase/firestore';
 
 export default function Inventario({ navigation }: any) {
-    const categorias = ['Abarrotes', 'Canasta basica', 'Fruta y verdura', 'No alimenticios', 'Productos animales']
+
+    const { db } = useFirebase();
+    const [categorias, setCategorias] = useState<any[]>([]);
+
+    useEffect(() => {
+        if (db) {
+            const getCategorias = async () => {
+                const docRef = doc(db, 'Inventario', 'Categorias');
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    const data = docSnap.data();
+                    const categoriasArray: any[] = data.categorias || [];
+                    console.log(categoriasArray);
+                    setCategorias(categoriasArray);
+                } else {
+                    console.log("No such document!");
+                }
+            };
+            getCategorias();
+        }
+    }, [db]);
+
     return (
         <View style={styles.container}>
             {
                 categorias.map((categoria, index) => {
+                    console.log(categoria);
                     return (
                         <View key={index}>
                             <CategoryItem title={categoria} navigation={navigation} />
